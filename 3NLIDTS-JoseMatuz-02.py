e15375853410fae21f8f375f97adbb4d434dff50
ProtoFurOwO <156768164+ProtoFurOwO@users.noreply.github.com>
2024-08-14 19:08:10 -0600

    Add files via upload
    codigo de python

---
+++ 3NLIDTS-JoseMatuz-02.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 10:53:26 2024
+
+@author: ProtofurOwO
+"""
+
+from tkinter import *
+ventana = Tk()
+ventana.title("Primera Ventana en python")
+ventana.geometry("520x480")
+ventana.mainloop()
